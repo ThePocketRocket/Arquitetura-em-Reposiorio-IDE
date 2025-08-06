e703fba5b8c2dae00aab4d0bb51e3c4b581c53c3
Daniel Ferreira <pocketrocket@outlook.com.br>
2025-08-06 00:04:17 -0300

    Retorno adicionado

---
+++ modules/highlighter.js
@@ -0,0 +1,10 @@
+module.exports = {
+    highlightsMap: () => {
+        return {
+            'exibe': 'orange',
+            '=': 'pink',
+            'inteiro': 'blue',
+            'texto': 'green'
+        }
+    }
+}
